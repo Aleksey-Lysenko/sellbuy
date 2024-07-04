4b6d72ee0cf084569473b95eae65f7e622a72852
Aleksey-Lysenko <165196926+Aleksey-Lysenko@users.noreply.github.com>
2024-07-04 22:19:56 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/buysell/configurations/SecurityConfig.java
+++ src/main/java/com/example/buysell/configurations/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,8 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
-        
-             http.csrf().disable();
+        http.csrf().disable();
     }
 
     @Override
